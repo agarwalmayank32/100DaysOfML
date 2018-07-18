f5ca5b16367709cf501c4c69e79514b5ecf9c358
mayank <agarwal.mayank32@gmail.com>
2018-07-18 22:18:56 +0530

    Day 7 and Day 8 log Commit with Code

---
+++ Code/Day 8/Algorithm Comparison for Siraj Rival Data.py
@@ -0,0 +1,53 @@
+import pandas
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+# [gender]
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+seed = 7
+# prepare models
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+results = []
+names = []
+scoring = 'accuracy'
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+
+	clf= model;
+	clf = clf.fit(X,Y)
+	prediction = clf.predict([[190,70,43]])
+
+	msg = "%s: %f (%f) %s" % (name, cv_results.mean(), cv_results.std(),prediction)
+	print(msg)
+
+# boxplot algorithm comparison
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
--- log.md
+++ log.md
@@ -27,3 +27,14 @@
 ### Day 6: July 16, 2018
 
 **Today's Progress**: Continued with Probability from [EDX](https://www.edx.org/course/introduction-probability-science-mitx-6-041x-2)
+
+### Day 7: July 17, 2018
+
+**Today's Progress**: Completed Probability Unit-1 from [EDX](https://www.edx.org/course/introduction-probability-science-mitx-6-041x-2)
+
+### Day 8: July 18, 2018
+
+**Today's Progress**: Using the code from the [Siraj Raval Video](https://www.youtube.com/watch?v=T5pRlIbr6gg&list=PL2-dafEMk2A6QKz1mrk1uIGfHkC1zZ6UU) built an example to predict the gender using different Algorithms like Logistic Regression, DecisionTreeClassifier, KNeighborsClassifier, LinearDiscriminantAnalysis, GaussianNB and SVC. 
+Learnt the shortfall of using the same(Complete) dataset for classification and testing from this [Video](https://www.youtube.com/watch?v=0pP4EwWJgIU). 
+Also tried Comparing Different Algorithms and found accuracy for the Algorithms on the [Breast Cancer Dataset](http://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_breast_cancer.html).  
+
